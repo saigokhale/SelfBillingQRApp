696d7b24893d363dc9a13e09549f0a780a948783
saigokhale <saigokhale2002@gmail.com>
2021-12-11 01:09:04 +0530

    initial commit

--- app/src/main/java/com/example/selfbillingqrapp/HomeActivity2.java
+++ app/src/main/java/com/example/selfbillingqrapp/HomeActivity2.java
@@ -1,26 +1,39 @@
 package com.example.selfbillingqrapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeActivity2 extends AppCompatActivity {
-    ImageButton QRButton;
+    Button QRButton;
+    int PERM_CODE = 11;
+    String[] permissions = {
+            Manifest.permission.CAMERA
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home2);
 
-        QRButton = (ImageButton) findViewById(R.id.qrbtn);
+        QRButton = (Button) findViewById(R.id.buttonQR);
 
         QRButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openQrActivity();
+//                if(checkpermissions())
+                    startActivity(new Intent(getApplicationContext(), QrActivity.class));
             }
         });
     }
@@ -29,4 +42,41 @@ public class HomeActivity2 extends AppCompatActivity {
         Intent intent_QR= new Intent(this, QrActivity.class);
         startActivity(intent_QR);
     }
+
+    private boolean checkpermissions(){
+        List<String> listofpermisssions = new ArrayList<>();
+        for (String perm: permissions){
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), perm) != PackageManager.PERMISSION_GRANTED){
+                listofpermisssions.add(perm);
+            }
+        }
+        if (!listofpermisssions.isEmpty()){
+            ActivityCompat.requestPermissions(this, listofpermisssions.toArray(new String[listofpermisssions.size()]), PERM_CODE);
+            return false;
+        }
+        return true;
+    }
+
 }
+
+
+
+//    Button btnScan;
+
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        btnScan = (Button) findViewById(R.id.button);
+//        btnScan.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(getApplicationContext(), ScannerActivity.class));
+//            }
+//        });
+//
+//    }
+
--- app/src/main/java/com/example/selfbillingqrapp/QrActivity.java
+++ app/src/main/java/com/example/selfbillingqrapp/QrActivity.java
@@ -1,14 +1,115 @@
 package com.example.selfbillingqrapp;
 
+import static com.budiyev.android.codescanner.CodeScanner.*;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class QrActivity extends AppCompatActivity {
+import com.budiyev.android.codescanner.AutoFocusMode;
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.budiyev.android.codescanner.ErrorCallback;
+import com.google.zxing.Result;
+
+import org.w3c.dom.Text;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import java.util.concurrent.ExecutionException;
+import java.util.regex.Pattern;
+
+//import me.dm7.barcodescanner.zbar.ZBarScannerView;
+
+public class QrActivity<permissions> extends AppCompatActivity
+{
+    TextView txt;
+    CodeScanner codeScanner;
+    CodeScannerView codeScannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr);
+
+        setPermission();
+        txt = (TextView) findViewById(R.id.textView);
+        codeScannerView = (CodeScannerView) findViewById(R.id.scannerView);
+        codeScanner = new CodeScanner(this, codeScannerView);
+
+
+        codeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //Toast.makeText(this, result.toString(),Toast.LENGTH_SHORT);
+                        txt.setText(result.getText());
+                        String temp=txt.getText().toString();
+                        Toast.makeText(QrActivity.this, temp,Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        codeScanner.startPreview();
+        requestCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        codeScanner.releaseResources();
+        super.onPause();
+    }
+
+    private void requestCamera() {
+        codeScanner.startPreview();
+    }
+
+    private void setPermission()
+    {
+        int permission= ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if(permission != PackageManager.PERMISSION_GRANTED)
+        {
+            makeRequest();
+        }
+    }
+
+    private void makeRequest() {
+        Pattern ptr = Pattern.compile(" ");
+        ActivityCompat.requestPermissions(this, ptr.split(Manifest.permission.CAMERA.toString()), 103);
+
+    }
+
+
+    public void OnRequestPermissionsResultCallback(int requestCode,String[] permissions, int[] grantResults)
+    {
+        if(requestCode==103)
+        {
+            if(grantResults.length==0||grantResults[0]!=PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(this, "You need to enable Camera permissions",Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                //successful
+            }
+        }
     }
 }
